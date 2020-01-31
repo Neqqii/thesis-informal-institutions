96dc0722cae98a4effcee54ea6f40f868da1b73a
Neqqii <nvborodin.nik@gmail.com>
2020-01-31 11:28:41 +0300

    add datasets

--- Research/research.py
+++ Research/research.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 
 os.chdir('C:\\Users\\Nvmind\\github\\thesis-informal-institutions\\Research\\Datasets')
+
+# Импорт датасета (GDP growth)
 aux_df1 = pd.read_csv('gdp_growth_90to19.csv')
 
 aux_colnames1 = aux_df1.columns
@@ -19,3 +21,47 @@ gdp_annual_growth = aux_df1[['Country Name', 'Country Code',
                              '2018 [YR2018]', '2019 [YR2019]']]
 
 print(gdp_annual_growth)
+
+# Импорт датасета (population)
+aux_df2 = pd.read_csv('population_90to19.csv')
+
+aux_colnames2 = aux_df2.columns
+print(aux_colnames2)
+
+# Оставляем нужные столбцы
+pops = aux_df2[['Country Name', 'Country Code',
+                '1990 [YR1990]', '2000 [YR2000]',
+                '2010 [YR2010]', '2017 [YR2017]',
+                '2018 [YR2018]', '2019 [YR2019]']]
+
+print(pops)
+
+# Импорт датасета (emigration)
+aux_df3 = pd.read_csv('tps00177.csv')
+
+aux_colnames3 = aux_df3.columns
+print(aux_colnames3)
+
+# Оставляем нужные столбцы
+emigration_total = aux_df3[['geo',
+                            '2006 ', '2007 ', '2008 ', '2009 ',
+                            '2010 ', '2011 ', '2012 ', '2013 ',
+                            '2014 ', '2015 ',
+                            '2016 ', '2017 ']]
+
+print(emigration_total)
+
+# Импорт датасета (immigration)
+aux_df4 = pd.read_csv('tps00176.csv')
+
+aux_colnames4 = aux_df4.columns
+print(aux_colnames4)
+
+# Оставляем нужные столбцы
+immigration_total = aux_df4[['geo',
+                             '2006 ', '2007 ', '2008 ', '2009 ',
+                             '2010 ', '2011 ', '2012 ', '2013 ',
+                             '2014 ', '2015 ',
+                             '2016 ', '2017 ']]
+
+print(immigration_total)
